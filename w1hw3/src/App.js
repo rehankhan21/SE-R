b5c47c6f5da57c360dbd8d68a88516ec2a6ddf4c
rehankhan21 <narutop224@gmail.com>
2021-05-10 12:40:46 -0400

    tardis hw finished

--- w1hw3/src/App.js
+++ w1hw3/src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
 import Tardis from './AppComp';
+import DivOne from './DivOne';
+import DivThree from './DivThree';
+import DivTwo from './DivTwo';
 
 function App() {
   return (
     <div className="App">
-      <Tardis />
+      {/* <Tardis /> */}
+      <DivThree />
     </div>
   );
 }
