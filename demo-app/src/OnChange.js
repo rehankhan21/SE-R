b358bba782b9ec165a80648ae34cf2efab48c41b
rehankhan21 <narutop224@gmail.com>
2021-05-10 10:20:36 -0400

    OnChange class with event.target

--- demo-app/src/OnChange.js
+++ demo-app/src/OnChange.js
@@ -1,35 +1,47 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class OnChange extends Component {
 
     constructor(props) {
         super(props);
 
-        this.state = {name : ""};
-        this.handleChange = this.handleChange.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
-    } 
+        this.state = { name: "", area: "" };
+        this.handleCha = this.handleChange.bind(this)
+        this.handleChaArea = this.handleChangeArea.bind(this)
+        this.handleSub = this.handleSubmit.bind(this)
+    }
 
     handleChange(event) {
 
-        this.setState({name : event.target.value});
+        this.setState({ name: event.target.value});
+
+    }
+
+    handleChangeArea(event) {
+        
+        this.setState({ area: event.target.value})
     }
 
     handleSubmit(event) {
-        alert("A name was submitted: " + this.state.name);
+        alert("A name was submitted: " + this.state.name  );
+        alert(this.state.name + "'s essay was submitted: " + this.state.area);
         event.preventDefault();
     }
 
-    render () {
+    render() {
 
         return (
-            <form onSubmit = {this.handleSubmit}>
-            <label>
-                Name:
-                <input type="text" value={this.state.value} onChange={this.handleChange}></input>
-            </label>
-            <input type="submit" value="Submit"></input>
-        </form>
+            <form onSubmit={this.handleSub}>
+                <label>
+                    Name:
+                    <input type="text" value={this.state.name} onChange={this.handleCha}></input>
+                </label>
+                <label>
+                    Essay:
+                    <textarea value={this.state.area} onChange={this.handleChaArea}></textarea>
+                </label>
+                <input type="submit" value="Submit"></input>
+            </form>
         )
     }
 
