a51ef5ebc36380132e6d94d14b10231d008a9fc2
rehankhan21 <narutop224@gmail.com>
2021-05-26 14:22:59 -0400

    fixed display list component

--- mod2project/src/DisplayList.js
+++ mod2project/src/DisplayList.js
@@ -1,6 +1,14 @@
 import React from 'react'
 
 function DisplayList(props) {
+    for(let i = 0; i <= 5; i++){
+        return <div>
+            <div>{JSON.stringify(props.list[i].title)}</div>
+            <img src={props.list[i].image_url}></img>
+            <div>{props.list[i].synopsis}</div>
+            <a href={props.list[i].url}>myanimelist</a>
+        </div>
+    };
     return (
         <div>
             {
