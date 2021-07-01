229cc4d5c2ec92eca75cba80f86d51e1bd5e23bd
rehankhan21 <narutop224@gmail.com>
2021-07-01 08:23:36 -0400

    added delete action button

--- react_router/src/App.js
+++ react_router/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom"
 import Nav from './Nav';
